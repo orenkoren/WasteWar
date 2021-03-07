4b9f1c617d725e6295ca5fee3d8c74c947435854
orenkoren <oren-k@hotmail.com>
2021-03-07 21:16:34 +0200

    Grid 0

---
+++ WasteWar/Assets/Scripts/Grid/GridCreator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GridCreator : MonoBehaviour
+{
+    public Transform gridLocation;
+    public LayerMask ground;
+    private BuildingGrid grid;
+    private Ray ray;
+    private RaycastHit rayHit;
+
+    void Start()
+    {
+        grid = new BuildingGrid(4, 2, 10f, gridLocation);
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(ray.origin, rayHit.point);
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out rayHit, Mathf.Infinity, ground);
+            print(rayHit.point);
+            grid.SetValue(rayHit.point, 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+    }
+}
