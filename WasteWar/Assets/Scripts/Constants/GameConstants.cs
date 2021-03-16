7156020aafe672142360e82e5f4329c83483be52
BogdanK0451 <zeljio@gmail.com>
2021-03-16 15:17:27 +0100

    comment removal

--- WasteWar/Assets/Scripts/Constants/GameConstants.cs
+++ WasteWar/Assets/Scripts/Constants/GameConstants.cs
@@ -5,8 +5,7 @@ namespace Constants
     public class GameConstants : MonoBehaviour
     {
         public Vector3 DEFAULT_OBJECT_ROTATION;
-        // Start is called before the first frame update
-        //what's this here?
+
         public static GameConstants Instance { get; private set; }
 
         private void Awake()
@@ -14,7 +13,6 @@ namespace Constants
             if (Instance == null)
             {
                 Instance = this;
-                //what's this here?
                 DontDestroyOnLoad(gameObject);
             }
             else
