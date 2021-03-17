38938825cbd45243700f315963c88a81f762b41d
orenkoren <43620896+orenkoren@users.noreply.github.com>
2021-03-17 23:05:40 +0200

    Hellshields (#9)
    * AI Pathfinding
    
    * const fix
    
    * zel.unity scene fixes
    
    * readme change
    
    * added scene
    
    * Grid 0
    
    * camera work in progress
    
    * finished implementing basic camera movement
    
    * fixes to the camera
    
    * refactoring
    
    * refactoring + fix an out of bounds camera bug
    
    * comment removal
    
    * comment removal
    
    * fixed camera smoothness
    
    * fixed zel.unity merge conflicts
    
    * removed magic number
    
    * safepoint
    
    * wip
    
    * safepoint, wip
    
    * safepoint, wip
    
    * safepoint, wip
    
    * implemented functional logical and visual grid, including structure placement
    
    * safepoint
    
    * major refactor
    
    * comment removal
    
    * Camera.main refactor
    
    * fixed the scene so that the program works after merge
    
    * adjusted turret location on the scene
    
    Co-authored-by: orenkoren <oren-k@hotmail.com>
    
    * Cleanup
    
    * scene and prefab apply
    
    * GameEvents example
    
    Co-authored-by: BogdanK0451 <zeljio@gmail.com>

---
+++ WasteWar/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public static class GameEvents
+{
+    public static event EventHandler<DrawData> DrawStateChangedListeners;
+    public static void FireDrawStateChanged(object sender, DrawData structurePrefab) => 
+                                            DrawStateChangedListeners?.Invoke(sender, structurePrefab);
+}
+
+public class DrawData
+{
+    public GameObject StructurePrefab { get; set; }
+    public Vector3 StructureLocation { get; set; }
+
+}
