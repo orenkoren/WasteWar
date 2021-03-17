fea51203e509522cc13f938d92ccbd4c72b757d4
BogdanK0451 <zeljio@gmail.com>
2021-03-17 14:14:26 +0100

    zel.unity scene fixes
    * readme change
    
    * added scene
    
    * Grid 0
    
    * camera work in progress
    
    * finished implementing basic camera movement
    
    * fixes to the camera
    
    * refactoring
    
    * refactoring + fix an out of bounds camera bug
    
    * comment removal
    
    * comment removal
    
    * fixed camera smoothness
    
    * fixed zel.unity merge conflicts
    
    * removed magic number
    
    * safepoint
    
    * wip
    
    * safepoint, wip
    
    * safepoint, wip
    
    * safepoint, wip
    
    * implemented functional logical and visual grid, including structure placement
    
    * safepoint
    
    * major refactor
    
    * comment removal
    
    * Camera.main refactor
    
    * fixed the scene so that the program works after merge
    
    * adjusted turret location on the scene
    
    Co-authored-by: orenkoren <oren-k@hotmail.com>

--- WasteWar/Assets/Scripts/Constants/GameKeys.cs
+++ WasteWar/Assets/Scripts/Constants/GameKeys.cs
@@ -4,10 +4,8 @@ namespace Constants
 {
     public class GameKeys : MonoBehaviour
     {
-        public KeyCode[] StructureKeybinds { get; private set; } = new KeyCode[] { KeyCode.B, KeyCode.C, KeyCode.Escape };
+        public KeyCode[] StructureKeybinds { get; private set; } = new KeyCode[] { KeyCode.B, KeyCode.C,KeyCode.X, KeyCode.Escape };
         //KeyCode[] MenuKeybinds = new KeyCode[] { KeyCode.Escape };
-
-
         public static GameKeys Instance { get; private set; }
 
         private void Awake()
