19176d43548686f24bc2964716cc7f1161087c3e
orenkoren <oren-k@hotmail.com>
2021-04-10 03:46:19 +0300

    collision jobs

--- WasteWar/Assets/Scripts/AI/Systems/AICollisionSystem.cs
+++ WasteWar/Assets/Scripts/AI/Systems/AICollisionSystem.cs
@@ -1,9 +1,10 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Physics;
 using Unity.Physics.Systems;
-using UnityEngine;
+using Unity.Rendering;
 
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 [UpdateAfter(typeof(EndFramePhysicsSystem))]
@@ -11,13 +12,16 @@ public class AICollisionSystem : SystemBase
 {
     BuildPhysicsWorld m_BuildPhysicsWorldSystem;
     StepPhysicsWorld m_StepPhysicsWorldSystem;
+    EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
     EntityQuery m_TriggerGravityGroup;
+    NativeList<Entity> entitiesToDestroy = new NativeList<Entity>(Allocator.Persistent);
 
     protected override void OnCreate()
     {
         base.OnCreate();
         m_BuildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
         m_StepPhysicsWorldSystem = World.GetOrCreateSystem<StepPhysicsWorld>();
+        m_EndSimulationEcbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnStartRunning()
@@ -41,9 +45,27 @@ public class AICollisionSystem : SystemBase
 
         Dependency = new CollisionJob
         {
-            PhysicsVelocityGroup = GetComponentDataFromEntity<PhysicsVelocity>()
+            PhysicsVelocityGroup = GetComponentDataFromEntity<PhysicsVelocity>(),
+            EntitiesList = entitiesToDestroy
+
         }.Schedule(m_StepPhysicsWorldSystem.Simulation,
             ref m_BuildPhysicsWorldSystem.PhysicsWorld, Dependency);
+
+        var entities = entitiesToDestroy;
+        var ecb = m_EndSimulationEcbSystem.CreateCommandBuffer();
+
+        var job = Job
+            .WithBurst()
+            .WithCode(() =>
+            {
+                foreach (var entity in entities)
+                {
+                    ecb.DestroyEntity(entity);
+
+                }
+                entities.Clear();
+            }).Schedule(Dependency);
+        m_EndSimulationEcbSystem.AddJobHandleForProducer(job);
     }
 
     [BurstCompile]
@@ -51,9 +73,32 @@ public class AICollisionSystem : SystemBase
     {
         public ComponentDataFromEntity<PhysicsVelocity> PhysicsVelocityGroup;
 
+        public NativeList<Entity> EntitiesList;
+
         public void Execute(CollisionEvent collisionEvent)
         {
-            //Debug.Log("collision");
+            // Check if one of the colliding entities is Attacker, if so, destroy it
+            Entity entityA = collisionEvent.EntityA;
+            Entity entityB = collisionEvent.EntityB;
+
+            bool isBodyADynamic = PhysicsVelocityGroup.HasComponent(entityA);
+            bool isBodyBDynamic = PhysicsVelocityGroup.HasComponent(entityB);
+
+            if (isBodyADynamic)
+            {
+                EntitiesList.Add(entityA);
+
+            }
+            else if (isBodyBDynamic)
+            {
+                EntitiesList.Add(entityB);
+            }
         }
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        entitiesToDestroy.Dispose();
     }
 }
