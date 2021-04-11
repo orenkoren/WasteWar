0d1eb6db2def4f91f24e78d78f1a8d85b790ddb9
BogdanK0451 <zeljio@gmail.com>
2021-04-11 18:17:54 +0200

    WIP

---
+++ WasteWar/Assets/Scripts/Data/PowerPoleData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPoleData : MonoBehaviour
+{
+    public int Health { get; private set; } = 100;
+    public int Armor { get; private set; } = 1;
+    public float Range { get; private set; } = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ WasteWar/Assets/Scripts/Drawing/Debug/ZoomRouteDraw.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ZoomRouteDraw : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] controlPoints;
+    
+    private Vector3 gizmosPosition;
+    private const float INCREMENT_BY= 0.02f;
+
+    private void OnDrawGizmos()
+    {
+        for (float t=0; t <= 1; t += INCREMENT_BY)
+        {
+            gizmosPosition = MathUtils.CalcCurrPosAlongTheCurve(t, controlPoints[0].position, controlPoints[1].position, controlPoints[2].position, controlPoints[3].position);
+            Gizmos.DrawSphere(gizmosPosition, 0.15f);
+        }
+    }
+}
