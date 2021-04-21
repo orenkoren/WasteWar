5e197775c85c7060aae63cfef69e3478ba7d15ec
BogdanK0451 <zeljio@gmail.com>
2021-04-21 19:20:32 +0200

    implemented pipe traversal logic
    next need to create pipeline from building A to building B if the traversal from one end to the other is successful

--- WasteWar/Assets/Scripts/Data/Methods/PipeLogic.cs
+++ WasteWar/Assets/Scripts/Data/Methods/PipeLogic.cs
@@ -61,79 +61,99 @@ public class PipeLogic : MonoBehaviour
             return pipes.TopLeft;
 
         return template;
+
+        bool CheckForCondition(Ray rayDirection, out RaycastHit hit, Vector3 directedUnitVector)
+        {
+            return
+            Physics.Raycast(rayDirection, out hit, 1f, LayerMasks.Instance.ATTACKABLE)
+            && hit.collider.gameObject.tag.Contains("Pipe")
+            && CheckIfPipeAligns(directedUnitVector, hit.collider.gameObject.GetComponent<PipeState>().activeSides)
+            ? true : false;
+        }
     }
 
     private void TraversePipes(object sender, GameObject structure)
     {
         ActiveSides activeSides = structure.GetComponent<PipeState>().activeSides;
-        GameObject firstDirection = null;
-        GameObject SecondDirection = null;
+        Vector3 direction1 = Vector3.zero;
+        Vector3 direction2 = Vector3.zero;
+        GameObject structure1 = structure;
+        GameObject structure2 = structure;
 
+        if (activeSides.IsTop == true && direction1 == Vector3.zero)
+            direction1 = Vector3.forward;
+        else if (activeSides.IsTop == true)
+            direction2 = Vector3.forward;
+
+        if (activeSides.IsRight == true && direction1 == Vector3.zero)
+            direction1 = Vector3.right;
+        else if (activeSides.IsRight == true)
+            direction2 = Vector3.right;
+
+        if (activeSides.IsBottom == true && direction1 == Vector3.zero)
+            direction1 = Vector3.back;
+        else if (activeSides.IsBottom == true)
+            direction2 = Vector3.back;
+
+        if (activeSides.IsLeft == true && direction1 == Vector3.zero)
+            direction1 = Vector3.left;
+        else if (activeSides.IsLeft == true)
+            direction2 = Vector3.left;
+
+        int i1 = 0;
         do
         {
-
+            structure1= NextPipe(structure1, ref direction1);
+            Debug.Log("dir1 " + i1);
+            i1++;
         }
-        while (firstDirection != null || !firstDirection.tag.Contains("Building"));
+        while (structure1 != null && !structure1.tag.Equals("Building"));
 
-
-        //if (!(structure.tag.Contains("Building"))){
-        //    ConstructPipeline(sender, structure);
-        //    ConstructPipeline(sender, structure);
-        //}
-
+        int i2 = 0;
+        do
+        {
+            structure2 = NextPipe(structure2, ref direction2);
+            Debug.Log("dir2 "+i2);
+            i2++;
+        }
+        while (structure2 != null && !structure2.tag.Equals("Building"));
     }
 
-    private GameObject Helper(GameObject pipe,ActiveSides activeSides)
+    private GameObject NextPipe(GameObject pipe, ref Vector3 dir)
     {
-        bool isUp = false;
-        bool isRight = false;
-        bool isDown = false;
-        bool isLeft = false;
-        activeSides.
-
-        if(activeSides)
-
-
         Vector3 pipeCenter = new Vector3(pipe.transform.position.x, pipe.transform.position.y, pipe.transform.position.z);
         RaycastHit hit;
-        Ray rayUp = new Ray(pipeCenter, Vector3.forward);
-        Ray rayRight = new Ray(pipeCenter, Vector3.right);
-        Ray rayDown = new Ray(pipeCenter, Vector3.back);
-        Ray rayLeft = new Ray(pipeCenter, Vector3.left);
+        Ray ray = new Ray(pipeCenter, dir);
+        ActiveSides activeSides;
 
+        if (Physics.Raycast(ray, out hit, 1f, LayerMasks.Instance.ATTACKABLE))
+        {
+            if (hit.collider.gameObject.tag.Contains("Pipe"))
+            {
+                dir=GetNextDirection(dir);
+                return hit.collider.gameObject;
+            }
+            else if (hit.collider.gameObject.tag.Equals("Building"))
+                return hit.collider.gameObject;
 
-        if (CheckForCondition(rayUp, out hit, Vector3.forward))
-            isUp = true;
-        if (CheckForCondition(rayRight, out hit, Vector3.right))
-            isRight = true;
-        if (CheckForCondition(rayDown, out hit, Vector3.back))
-            isDown = true;
-        if (CheckForCondition(rayLeft, out hit, Vector3.left))
-            isLeft = true;
+            return null;
+        }
+        return null;
 
-        if (isUp && isDown)
-            return pipes.TopBottom;
-        else if (isLeft && isRight)
-            return pipes.LeftRight;
-        else if (isUp && isRight)
-            return pipes.TopRight;
-        else if (isDown && isRight)
-            return pipes.BottomRight;
-        else if (isDown && isLeft)
-            return pipes.BottomLeft;
-        else if (isUp && isLeft)
-            return pipes.TopLeft;
+        Vector3 GetNextDirection(Vector3 dir)
+        {
+            activeSides = hit.collider.gameObject.GetComponent<PipeState>().activeSides;
+            if (activeSides.IsTop && dir != Vector3.back)
+                dir = Vector3.forward;
+            else if (activeSides.IsRight && dir != Vector3.left)
+                dir = Vector3.right;
+            else if (activeSides.IsBottom && dir != Vector3.forward)
+                dir = Vector3.back;
+            else if (activeSides.IsLeft && dir != Vector3.right)
+                dir = Vector3.left;
 
-        return template;
-    }
-
-    private bool CheckForCondition(Ray rayDirection,out RaycastHit hit,Vector3 directedUnitVector)
-    {
-       return 
-       Physics.Raycast(rayDirection, out hit, 1f, LayerMasks.Instance.ATTACKABLE)
-       && hit.collider.gameObject.tag.Contains("Pipe")
-       && CheckIfPipeAligns(directedUnitVector, hit.collider.gameObject.GetComponent<PipeState>().activeSides) 
-       ? true : false;
+            return dir;
+        }
     }
 
     private void OnDestroy()
