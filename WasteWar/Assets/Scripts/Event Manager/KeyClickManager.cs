0f25f9dcf5ba6e9f2d52e4a53dc1dd296727b2b5
orenkoren <oren-k@hotmail.com>
2021-04-30 22:21:46 +0300

    made map bigger

--- WasteWar/Assets/Scripts/Event Manager/KeyClickManager.cs
+++ WasteWar/Assets/Scripts/Event Manager/KeyClickManager.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using Constants;
+﻿using Constants;
+using UnityEngine;
 
 public class KeyClickManager : MonoBehaviour
 {
@@ -27,38 +27,37 @@ public class KeyClickManager : MonoBehaviour
             if (Input.GetKeyDown(key))
             {
                 ray = cam.ScreenPointToRay(Input.mousePosition);
-
                 // if mouse on the ground/ and cursor within bounds
-                if (Physics.Raycast(ray, out hit, CameraConstants.Instance.RAYCAST_DISTANCE, LayerMasks.Instance.GROUND) 
+                if (Physics.Raycast(ray, out hit, CameraConstants.Instance.RAYCAST_DISTANCE, LayerMasks.Instance.GROUND)
                     && MathUtils.CursorIsWithinBounds(hit.point, terrain.terrainData.size))
                 {
                     switch (key)
                     {
                         case KeyCode.B:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Building, MousePos = hit.point});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Building, MousePos = hit.point });
                             break;
                         //TODO turret placement doesn't work (other placements do) (because of Turret components or something)... fix.
                         case KeyCode.V:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Turret, MousePos = hit.point});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Turret, MousePos = hit.point });
                             break;
                         case KeyCode.C:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Wall, MousePos = hit.point});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.Wall, MousePos = hit.point });
                             break;
                         case KeyCode.X:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = prefabPipe, MousePos = hit.point});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = prefabPipe, MousePos = hit.point });
                             break;
                         case KeyCode.Z:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.PowerPole, MousePos = hit.point});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = templates.PowerPole, MousePos = hit.point });
                             break;
                         case KeyCode.Escape:
-                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = null, MousePos = new Vector3( 0, 0, 0 )});
+                            GameEvents.FireTemplateSelected(this, new TemplateData { TemplateStructure = null, MousePos = new Vector3(0, 0, 0) });
                             break;
                     }
                 }
             }
         }
         if (Input.GetKeyDown(KeyCode.R))
-            GameEvents.FireBuildingRotation(this,5);
+            GameEvents.FireBuildingRotation(this, 5);
     }
 
     private void SetPipePrefab(object sender, string prefabName)
