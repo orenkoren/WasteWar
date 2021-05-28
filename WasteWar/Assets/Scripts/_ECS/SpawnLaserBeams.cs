5f884fa05ea9708958ee690dc3d031bb5cc44628
orenkoren <oren-k@hotmail.com>
2021-05-29 00:28:54 +0300

    laser cannon sound bank and random pitch

--- WasteWar/Assets/Scripts/_ECS/SpawnLaserBeams.cs
+++ WasteWar/Assets/Scripts/_ECS/SpawnLaserBeams.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class SpawnLaserBeams : MonoBehaviour
 {
     public AudioSource soundSource;
+    public AudioClip[] laserSounds;
 
 
     public void PlayBeamSound()
     {
         if (soundSource)
+        {
+            soundSource.pitch = Random.Range(0.8f, 1.2f);
+            soundSource.clip = laserSounds[Random.Range(0, laserSounds.Length - 1)];
             soundSource.Play();
+        }
     }
 }
